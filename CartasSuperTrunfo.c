58ace8d4829f000e19c5e74196815a337624d98a
Lobo <88998991+Lobooooooo14@users.noreply.github.com>
2025-01-25 18:01:47 -0300

    novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main()
+{
+    char city_code[5];
+    char city_name[50];
+    int city_population;
+    float city_area;
+    float city_GDP;
+    int city_tourist_attractions;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Digite o código da cidade: ");
+    scanf("%s", city_code);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s", city_name);
+
+    printf("Digite a população da cidade: ");
+    scanf("%d", &city_population);
+
+    printf("Digite a área da cidade: ");
+    scanf("%f", &city_area);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &city_GDP);
+
+    printf("Digite o número de pontos turísticos da cidade: ");
+    scanf("%d", &city_tourist_attractions);
+
+    printf(
+        "\nCidade: %s - [%s]\nPopulação: %d\nÁrea: %f\nPIB: %f\nPontos Turísticos: %d\n",
+        city_name,
+        city_code,
+        city_population,
+        city_area,
+        city_GDP,
+        city_tourist_attractions);
 
     return 0;
 }
