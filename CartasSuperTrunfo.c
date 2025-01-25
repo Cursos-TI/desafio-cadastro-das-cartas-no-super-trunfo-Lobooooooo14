1cb322ce62816cb397f557e0b577c5af07d12ad7
Lobo <88998991+Lobooooooo14@users.noreply.github.com>
2025-01-25 18:02:45 -0300

    aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,8 +6,11 @@ int main()
     char city_name[50];
     int city_population;
     float city_area;
-    float city_GDP;
+    float city_GDP_total;
     int city_tourist_attractions;
+
+    float city_population_density;
+    float city_GDP_per_capita;
 
     printf("Digite o código da cidade: ");
     scanf("%s", city_code);
@@ -22,19 +25,24 @@ int main()
     scanf("%f", &city_area);
 
     printf("Digite o PIB da cidade: ");
-    scanf("%f", &city_GDP);
+    scanf("%f", &city_GDP_total);
 
     printf("Digite o número de pontos turísticos da cidade: ");
     scanf("%d", &city_tourist_attractions);
 
+    city_population_density = city_population / city_area;
+    city_GDP_per_capita = city_GDP_total / city_population;
+
     printf(
-        "\nCidade: %s - [%s]\nPopulação: %d\nÁrea: %f\nPIB: %f\nPontos Turísticos: %d\n",
+        "\nCidade: %s - [%s]\nPopulação: %d\nÁrea: %.2f\nPIB: %.2f\nPontos Turísticos: %d\nDensidade de População: %.2f\nPIB por Capita: %.2f\n",
         city_name,
         city_code,
         city_population,
         city_area,
-        city_GDP,
-        city_tourist_attractions);
+        city_GDP_total,
+        city_tourist_attractions,
+        city_population_density,
+        city_GDP_per_capita);
 
     return 0;
 }
